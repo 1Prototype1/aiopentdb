56bcfc2862b41978b734dca633b34be2f9dd121e
CyCanCode <cycancode@gmail.com>
2020-08-24 13:55:30 +0700

    Improve examples

--- examples/basic.py
+++ examples/basic.py
@@ -1,22 +1,3 @@
-# aiopentdb
-
-![GitHub](https://img.shields.io/github/license/CyCanCode/aiopentdb?color=blue)
-![Python](https://img.shields.io/badge/python-3.7%20%7C%203.8-blue)
-![PyPI](https://img.shields.io/pypi/v/aiopentdb?color=blue)
-
-Async Python wrapper for OpenTDB API
-
-# Installing
-
-**Python 3.7 or higher is required**
-
-```sh
-pip install -U aiopentdb
-```
-
-# Quick Example
-
-```py
 import asyncio
 
 import aiopentdb
@@ -44,6 +25,3 @@ async def main():
 
 
 asyncio.run(main())
-```
-
-Look at [examples](examples) directory for more examples.
